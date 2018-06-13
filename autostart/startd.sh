7d1d321eb5f7cd4838e7ed0e8a9766a8318353cc
zyobum <zyobum@5068.info>
2018-06-14 00:22:27 +0800

    add config files

---
+++ autostart/startd.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+TITLE="miner"
+/bin/sleep 5
+source /home/user/.bashrc
+
+/usr/bin/tmux new-session -d -s $TITLE
+
+/usr/bin/tmux send-keys -t $TITLE "source /home/bitcoin/.bashrc" C-m
+/usr/bin/tmux send-keys -t $TITLE "cd" C-m
+/usr/bin/tmux send-keys -t $TITLE "cd miner" C-m
+/usr/bin/tmux send-keys -t $TITLE "excavator -c current.json" C-m
