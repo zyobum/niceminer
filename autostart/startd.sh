9efb174e09000dc103cdf95999b860cc1aa4885b
zyobum <zyobum@5068.info>
2018-11-05 19:33:35 +0800

    Update startd.sh

--- autostart/startd.sh
+++ autostart/startd.sh
@@ -6,7 +6,7 @@ source /home/user/.bashrc
 
 /usr/bin/tmux new-session -d -s $TITLE
 
-/usr/bin/tmux send-keys -t $TITLE "source /home/bitcoin/.bashrc" C-m
+/usr/bin/tmux send-keys -t $TITLE "source ~/.bashrc" C-m
 /usr/bin/tmux send-keys -t $TITLE "cd" C-m
 /usr/bin/tmux send-keys -t $TITLE "cd niceminer" C-m
 /usr/bin/tmux send-keys -t $TITLE "cd mine" C-m
